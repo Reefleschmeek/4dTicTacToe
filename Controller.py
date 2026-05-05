5f148e1e9898feed380d089ad15499fa56b93189
Russell <reefleschmeek@gmail.com>
2026-05-04 19:47:02 -0700

    .

--- Controller.py
+++ Controller.py
@@ -1,1 +1,21 @@
-print('arst')
+
+class Player:
+
+    def __init__(self):
+        self.next_move: int = None
+
+    def query(self, board):
+        pass
+
+    def stop(self):
+        pass
+
+class Game:
+
+    def __init__(self):
+        self.x = 0b0
+        self.o = 0b0
+        self.turn = 1
+    
+    def query(self, player: Player):
+        return player.next_move
